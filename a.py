9588abe26554570817d5b0594657935316b33124
xorsys11 <39439797+xorsys11@users.noreply.github.com>
2020-02-10 22:40:25 +0530

    Add files via upload
    1) added window to view rules
    2)added function to make iptables cmd

--- a.py
+++ a.py
@@ -1,26 +1,67 @@
 import PySimpleGUI as sg
 import os
-
+import subprocess
+import sys
 sg.theme('DarkAmber')  
 layout = [  [sg.Text('Change Permissions')],
-            [sg.Text('Enter IP'), sg.InputText()],
-            [sg.Button('Accept'), sg.Button('Clear')] ]
+            [sg.Text('Enter IP'), sg.InputText(key="ip")],
+            [sg.Button('Accept'), sg.Button('Clear')],
+            [sg.Button('View')],[sg.Output(size=(100,20),key="op")],
+            [sg.Exit()] ]
+
 window = sg.Window('Linux Firewall', layout)
+
+def runCommand(cmd, timeout=None, window=None):
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = ''
+    for line in p.stdout:
+        line = line.decode(errors='replace' if (sys.version_info) < (3, 5) else 'backslashreplace').rstrip()
+        output += line
+        print(line)
+        window.Refresh() if window else None        
+    retval = p.wait(timeout)
+    
+    return (retval, output)  
+
+
+def create_command(type1,ip,port,io,dport): 
+    command="iptables"
+    if type1!="":
+        command+=" "
+        command+=type1
+    if port!="":
+        command+=" -p "
+        command+=port
+    if dport!="":
+        command+=" --dport "
+        command+=dport
+    if ip!="":
+        command+=" -s "
+        command+=ip
+    if io!="":
+        command+=" -j "
+        command+=io
+    return command
+
+
 while True:
     event, values = window.read()
-    if event in (None,):  
+    if event in (None, 'Exit'):  
         break
     if event in ('Clear'):
-        cmd = 'sudo iptables -F'	
+        cmd = 'sudo iptables -F'
         os.system(cmd)
-        os.system('sudo iptables -L')
-        break
-    print('You entered ', values[0])
-    cm='sudo iptables -A INPUT -s '
-    ccm=' -j ACCEPT'
-    cam=cm+values[0]+ccm
-    print (cam)
-    os.system(cam)
-    os.system('sudo iptables -L')    
-window.close()
+        window.FindElement('op').Update('')
+        
+    if event in ('View'):
+        cmd = 'sudo iptables -L'
+        runCommand(cmd, window=window)
 
+
+    if event in ('Accept'):
+        cmd=""
+        cmd=create_command("-A INPUT",values["ip"],"","ACCEPT","")
+        runCommand(cmd, window=window)
+        print(cmd)
+window.close()
+    
