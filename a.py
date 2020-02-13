72d96e158c49168c50f6bff980d67184dc29b0e8
Arindaam <42868650+Arindaam@users.noreply.github.com>
2020-02-13 10:48:21 -0500

    Update a.py

--- a.py
+++ a.py
@@ -7,11 +7,10 @@ layout = [  [sg.Text('Change Permissions')],
             [sg.Text('IP'), sg.InputText(key="ip",size=(20,1)),
             sg.Text('Protocol'), sg.InputText(key="protocol",size=(10,1)),sg.Text('Destination Port'), sg.InputText(key="dport",size=(10,1))],
             [sg.InputCombo(('ACCEPT', 'REJECT','DROP'),default_value='ACCEPT',enable_events=True, key='action',size=(20,3)),
-            sg.Button('Test'),sg.Button('Clear')],
-            [sg.Button('View')],[sg.Output(size=(100,20),key="op")],
-            [sg.Exit()] ]
+            sg.Button('Test'),sg.Button('Flush')],
+            [sg.Button('View')],[sg.Output(size=(100,20),key="op")],[sg.Button('Clear')]]   
 
-window = sg.Window('Linux Firewall', layout)
+window = sg.Window('Linux Firewall', layout)    
 
 def runCommand(cmd, timeout=None, window=None):
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -48,17 +47,15 @@ def create_command(type1,ip,protocol,io,dport):
 
 while True:
     event, values = window.read()
-    if event in (None, 'Exit'):  
+    if event in (None,):  
         break
-    if event in ('Clear'):
+    if event in ('Flush'):
         cmd = 'sudo iptables -F'
         os.system(cmd)
-        window.FindElement('op').Update('')
         
     if event in ('View'):
         cmd = 'sudo iptables -L'
         runCommand(cmd, window=window)
-
 
     if event in ('Test'):
         cmd=""
@@ -66,5 +63,8 @@ while True:
         cmd=create_command("-A INPUT",values["ip"],values["protocol"],action,values["dport"])
         runCommand(cmd, window=window)
         print(cmd)
+        
+    if event in ('Clear'):   
+        window.FindElement('op').Update('')
 window.close()
     
